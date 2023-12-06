b38722b47a5f744c0be808ffa8e638acc061e177
Nixbu <83171543+Nixbu@users.noreply.github.com>
2023-12-07 00:23:47 +0200

    Implemented alpha-beta pruning in the minimax algorithm

--- utils/AI_Player.py
+++ utils/AI_Player.py
@@ -16,7 +16,7 @@ class AIPlayer:
             if sim_board.current_pits[move] != 0:
                 new_sim_board = sim_board.create_copy()
                 new_sim_board.make_move(move)
-                evaluation = minimax(new_sim_board, self.depth, False)
+                evaluation = minimax(new_sim_board, self.depth, False, -inf, inf)
 
                 if evaluation > best_eval:
                     best_eval = evaluation
@@ -24,9 +24,11 @@ class AIPlayer:
 
         return best_move
 
-def minimax(sim_board, depth, minimizing_player):
+def minimax(sim_board, depth, minimizing_player, alpha, beta):
     """
     Minimax algorithm to determine the best move in a mancala game
+    :param beta: The best (minimum) evaluation in the path
+    :param alpha: The best (maximum) evaluation in the path
     :param minimizing_player: To know if it needs to maximize or minimize evaluation
     :param sim_board: SimulatedBoard object, to simplify the board state
     :param depth: How many moves into the future to check
@@ -41,8 +43,11 @@ def minimax(sim_board, depth, minimizing_player):
             if sim_board.current_pits[move] != 0:
                 new_sim_board = sim_board.create_copy()
                 new_sim_board.make_move(move)
-                evaluation = minimax(new_sim_board, depth - 1, new_sim_board.turn)
+                evaluation = minimax(new_sim_board, depth - 1, new_sim_board.turn, alpha, beta)
                 min_eval = min(min_eval, evaluation)
+                beta = min(beta, min_eval)
+                if beta <= alpha:
+                    break
 
         return min_eval
 
@@ -52,8 +57,11 @@ def minimax(sim_board, depth, minimizing_player):
             if sim_board.current_pits[move] != 0:
                 new_sim_board = sim_board.create_copy()
                 new_sim_board.make_move(move)
-                evaluation = minimax(new_sim_board, depth - 1, new_sim_board.turn)
+                evaluation = minimax(new_sim_board, depth - 1, new_sim_board.turn, alpha, beta)
                 max_eval = max(max_eval, evaluation)
+                alpha = max(max_eval, alpha)
+                if beta <= alpha:
+                    break
 
         return max_eval
 
