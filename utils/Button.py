c923fef511d9052e411e937058a52f09f8caaf8e
Nixbu <83171543+Nixbu@users.noreply.github.com>
2023-12-07 23:45:36 +0200

    Added Button class

---
+++ utils/Button.py
@@ -0,0 +1,24 @@
+import pygame
+from .Settings import *
+
+class Button:
+    def __init__(self, screen, text, pos_x, pos_y):
+        self.screen = screen
+        self.text = default_font.render(text, 1, BLACK)
+        self.width = self.text.get_width()
+        self.height = self.text.get_height()
+        self.pos_x = pos_x
+        self.rect = pygame.Rect(pos_x, pos_y, self.width, self.height)
+        self.pos_y = pos_y
+
+    def draw(self):
+        self.screen.blit(self.text, (self.pos_x, self.pos_y))
+
+    def is_clicked(self, mouse_pos):
+        return self.rect.collidepoint(mouse_pos[0], mouse_pos[1])
+
+    def center(self):
+        self.pos_x = self.screen.get_width() // 2 - self.width // 2
+        self.rect.x = self.pos_x
+
+
